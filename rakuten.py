d8febdc6d8bec69be894149eb0ef2042ba25d4e5
MoonAndEye <MoonAndEye@users.noreply.github.com>
2015-07-29 11:11:07 +0900

    can work, but content need special treatment.

--- rakuten.py
+++ rakuten.py
@@ -3,17 +3,31 @@
 Created on Tue Jul 28 12:40:36 2015
 
 """
-
+"""
 import requests
 import datetime
 from bs4 import BeautifulSoup
+import certifi
 
 url = 'https://www.rakuten-sec.co.jp/web/market/ranking/value.html?ind=1&market=0&information=0'
 
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text.encode('shift-jis'))      # a `bytes` object
-text = data.decode('shift-jis') # a `str`; this step can't be used if data is binary
+response = requests.get(url, verify = False)
+soup = response.text.encode('shift-jis')    # a `bytes` object
+#text = data.decode('shift-jis') # a `str`; this step can't be used if data is binary
 #text = text[13:] #第13個是位
 
 print (soup)
+"""
+
+import urllib.request
+import re
+
+url = 'https://www.rakuten-sec.co.jp/web/market/ranking/value.html'
+
+req = urllib.request.Request(url)
+resp = urllib.request.urlopen(req)
+respData = resp.read()
+respData = respData.decode('utf-8')
+
+print (respData)
